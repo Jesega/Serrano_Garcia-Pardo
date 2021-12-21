4638056c2b43265f668b34806ea49ff7d4672ee7
Antonio Morales Fernandez <antonio.morafer@alum.uca.es>
2021-12-21 07:33:23 -0500

    Comentarios en cocheServiceImpl

--- src/main/java/evolsoft/concesionario/controller/CocheController.java
+++ src/main/java/evolsoft/concesionario/controller/CocheController.java
@@ -93,7 +93,9 @@ public class CocheController {
 		cocheService.newSell(soldCarDTO.getIdCoche(), soldCarDTO.getIdCliente(),
 		soldCarDTO.getIdVendedor());
 	}
-
+	/*
+	*Insertamos una lista en el servicio del coche
+	*/
 	@PostMapping("/insertList")
 	public void createList(@RequestBody List<CocheDTO> listCocheDto) {
 		cocheService.createList(listCocheDto);
