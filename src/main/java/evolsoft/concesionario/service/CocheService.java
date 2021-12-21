814becc986f7c0e02bac0e9775242224760e1c63
Jesús Serrano <jesus.serranogallan@alum.uca.es>
2021-12-21 11:50:15 +0100

    Añadido createList

--- src/main/java/evolsoft/concesionario/service/CocheService.java
+++ src/main/java/evolsoft/concesionario/service/CocheService.java
@@ -29,4 +29,6 @@ public interface CocheService {
 	public List<CocheDTO> findCochesInPriceRange(Integer minPrice, Integer maxPrice);
 
 	public List<CocheDTO> findCochesInStock();
+
+	void createList(List<CocheDTO> listCocheDto);
 }
